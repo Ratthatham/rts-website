7ebc2a19f6d81ff85a5ff1c149a0b41f1bb609cb
Ratthatham <ratthatham.siridol@gmail.com>
2022-09-08 00:03:55 +0700

    build: picture profile

--- src/component/home-section/home-section.js
+++ src/component/home-section/home-section.js
@@ -1,5 +1,7 @@
 import React from "react";
 import '../home-section/home-section.css'
+import profilePicture from '/Users/ratthathamsiridol/Documents/Fullstack Dev/rts-portfolio/src/asset/IMG_0263.png'
+
 
 const HomeSection = () => {
     return(
@@ -9,6 +11,13 @@ const HomeSection = () => {
                 <h1>Ratthatham Siridol</h1>
                 <h2>frontend web developer</h2>
             </div>
+            <div className="home-image">
+                <div className="img-box">
+                    <img src={profilePicture} alt="profile "/>
+                </div>
+            </div>
+            <a className="btn-download-resume" href="#"> Download Resume</a>
+            <a className="btn" href="#"> Let's Talk</a>
         </div>
     )
 }
